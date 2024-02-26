f5d6eb15a15bccf57e03cc300cd625517e38bab2
Koal M. Casler <71401318+KoalMCasler@users.noreply.github.com>
2024-02-26 12:33:07 -0400

    Done

---
+++ Assets/Scripts/UIManger.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIManger : MonoBehaviour
+{
+    public int Score;
+    public TextMeshProUGUI ScoreObject;
+    public float MaxRandom;
+    public float MinRandom;
+    void Start()
+    {
+        Score = 0;
+    }
+    void Update()
+    {
+        ScoreObject.text = string.Format("Score: {0:0,0}",Score);
+        CheckScore();
+    }
+    void CheckScore()
+    {
+        if(Score > 999999999)
+        {
+            Score = 999999999;
+        }
+        if(Score <= 0)
+        {
+            Score = 0;
+        }
+    }
+    public void UpdateScore(int Value)
+    {
+        if(Value != 0)
+        {
+            Score += Value;
+        }
+        if(Value == 0)
+        {
+            Score = 0;
+        }
+    }
+    public void AddRandomScore()
+    {
+        float RanValue = Random.Range(MinRandom, MaxRandom);
+        int PassValue = (int)RanValue;
+        UpdateScore(PassValue);   
+    }
+}
