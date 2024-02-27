839ea14556430115748c62638b9ea9e521a7aa2d
Koal M. Casler <71401318+KoalMCasler@users.noreply.github.com>
2024-02-27 10:23:20 -0400

    Added part 2

--- Assets/Scripts/UIManger.cs
+++ Assets/Scripts/UIManger.cs
@@ -7,18 +7,32 @@ using UnityEngine.SceneManagement;
 
 public class UIManger : MonoBehaviour
 {
+    [Header("Score Variables")]
     public int Score;
     public TextMeshProUGUI ScoreObject;
     public float MaxRandom;
     public float MinRandom;
+    [Header("Slider Variables")]
+    public Slider redSlider;
+    public Slider blueSlider;
+    public Slider greenSlider;
+    public Slider alphaSlider;
+    public GameObject sliderImmageObject;
+    public Image sliderImmage;
+    [Header("Key Hider Variables")]
+    public GameObject keyImmage;
+    public TextMeshProUGUI ShowHideText;
+    public bool keyIsHidden;
     void Start()
     {
+        keyIsHidden = false;
         Score = 0;
+        sliderImmage = sliderImmageObject.GetComponent<Image>();
     }
     void Update()
     {
-        ScoreObject.text = string.Format("Score: {0:0,0}",Score);
-        CheckScore();
+        CheckKeyIsHidden();
+        UpdateSliderColor();
     }
     void CheckScore()
     {
@@ -41,17 +55,48 @@ public class UIManger : MonoBehaviour
         {
             Score = 0;
         }
+        CheckScore();
+        ScoreObject.text = string.Format("Score: {0:0,0}",Score);
     }
     public void AddRandomScore()
     {
         float RanValue = Random.Range(MinRandom, MaxRandom);
         int PassValue = (int)RanValue;
-        UpdateScore(PassValue);   
+        UpdateScore(PassValue);
     }
     public void QuitGame()
     {
         //Debug line to test quit function in editor
         //UnityEditor.EditorApplication.isPlaying = false;
         Application.Quit();
+    }
+    void UpdateSliderColor()
+    {
+        Color sliderColor = new Color(redSlider.value,greenSlider.value,blueSlider.value,alphaSlider.value);
+        sliderImmage.color =  sliderColor;
+    }
+    public void ShowHideKey()
+    {
+        if(keyIsHidden == false)
+        {
+            keyIsHidden = true;
+        }
+        else
+        {
+            keyIsHidden = false;
+        }
+    }
+    void CheckKeyIsHidden()
+    {
+        if(keyIsHidden == false)
+        {
+            keyImmage.SetActive(true);
+            ShowHideText.text = "Hide Key";
+        }
+        else
+        {
+            keyImmage.SetActive(false);
+            ShowHideText.text = "Show Key";
+        }
     }
 }
